6690f17ccc07aa2805f41fac82f695ffd4169092
hazemessamm <hazemessamahmed@outlook.com>
2021-02-19 19:37:00 +0200

    added sequence class for training generators

--- kerax/utils.py
+++ kerax/utils.py
@@ -4,3 +4,12 @@ def to_categorical(inputs, num_classes=None):
     if num_classes is None:
         num_classes = max(inputs, axis=-1)
     return inputs[:, None] == arange(num_classes)
+
+
+
+class Sequence:
+    def __init__(self):
+        raise NotImplementedError
+
+    def __getitem__(self, index):
+        raise NotImplementedError
